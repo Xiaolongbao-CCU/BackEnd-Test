2ab0a583456ebf330929c1c17688aba7fc1b1729
LETS-STAR\user <ifhange@gmail.com>
2018-03-16 20:39:34 +0800

    add Server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var Http = require( 'http' ),
+    Router = require( 'router' ),
+    server,
+    router;
+router = new Router();
+ 
+server = Http.createServer( function( request, response ) {
+  router( request, response, function( error ) {
+    if ( !error ) {
+      response.writeHead( 404 );
+    } else {
+      // Handle errors
+      console.log( error.message, error.stack );
+      response.writeHead( 400 );
+    }
+    response.end( 'RESTful API Server is running!' );
+  });
+});
+ 
+server.listen( 3000, function() {
+  console.log( 'Listening on port 3000' );
+});
+
+var counter = 0,
+    todoList = {};
+
